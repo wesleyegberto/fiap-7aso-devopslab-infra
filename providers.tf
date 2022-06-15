a8ce5c58fc7daa00835dcb446df7701a39738435
Wesley Egberto <wesleyegberto@gmail.com>
2022-06-14 22:26:33 -0300

    adicionado provider e recurso GCP do Terraform

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.22.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/vagrant/gcpkey.json")
+
+  project = "fiap-study"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
