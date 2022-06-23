5e0dd2fae88add3fd09e7f8d1822b2d62e622087
Wesley Egberto <wesleyegberto@gmail.com>
2022-06-22 21:28:00 -0300

    adicionado criação do AppEngine e Registry no GCP

--- gcp-main.tf
+++ gcp-main.tf
@@ -20,3 +20,17 @@ resource "google_compute_instance" "firstvm" {
     }
   }
 }
+
+resource "google_app_engine_application" "app" {
+  project     = "fiap-study"
+  location_id = "us-central"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider      = google-beta
+
+  location      = "us-central1"
+  repository_id = "fiap-labdevops"
+  description   = "Imagens Docker"
+  format        = "DOCKER"
+}
